b2e97e8024afed25fc471b18831604dfbdedbb82
ExXiee <exxample912@gmail.com>
2025-08-28 00:57:47 +0700

    Update service-worker.js
    Bener

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='petualangan-19juta-plus-v1';
+const CACHE_NAME = 'petualangan-19juta-plus-v5';
 const ASSETS=['./','./index.html','./style.css','./main.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
